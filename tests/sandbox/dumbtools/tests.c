9741a3b97c504a8bdee5d20f1a540cfada9f436c
Korrmet <cyka.blyat.ebat.moy.huy@gmail.com>
2021-06-15 18:36:39 +0300

    added test coverage

--- tests/sandbox/dumbtools/tests.c
+++ tests/sandbox/dumbtools/tests.c
@@ -31,18 +31,18 @@ int main(int argc, char** argv)
 { TST_INIT();
 
   TST_START(dumbtools_memcpy, "simple call");
-    char destination[sizeof("source") + 2] =
+    unsigned char destination[sizeof("source") + 2] =
     { 0xaa, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x55 };
-    char check_array[sizeof("source") + 2] =
+    unsigned char check_array[sizeof("source") + 2] =
     { 0xaa, 's', 'o', 'u', 'r', 'c', 'e', 0x00, 0x55 };
     dumbtools_memcpy(&destination[1], "source", sizeof("source"));
     TST_ASS(memcmp(destination, check_array, sizeof(destination)) == 0);
   TST_END();
 
   TST_START(dumbtools_memset, "simple call");
-    char destination[10] = 
+    unsigned char destination[10] = 
     { 0xaa, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x55 };
-    char check_array[10] =
+    unsigned char check_array[10] =
     { 0xaa, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55 };
     dumbtools_memset(&destination[1], 0x00, 8);
     TST_ASS(memcmp(destination, check_array, sizeof(destination)) == 0);
